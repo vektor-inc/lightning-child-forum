bf74c320927d30f27849fa7ef08b36144fdb0dac
catherine <catherine5th.avenue@gmail.com>
2018-02-28 15:44:41 +0900

    #1　チェックがあったらタイトルに [解決済み] を追加

--- functions.php
+++ functions.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 	トピックを REST API で使えるようにする
  */
@@ -32,6 +31,66 @@ function vkf_topic_post_type_custom( $args ){
 		'rest_base' => 'topics',
 	);
 	$args = array_merge( $args, $rest_args );
-	return $args;
+	echo '<br />_|＼○_ﾋｬｯ ε=＼＿○ﾉ ﾎｰｳ!!'.'<br />'."\n";
 
+	return $args;
+}
+
+
+/*
+	トピックの状態のカスタムフィールドを追加
+*/
+if(function_exists("register_field_group"))
+{
+register_field_group(array (
+	'id' => 'acf_resolve',
+	'title' => 'トピックの状態',
+	'fields' => array (
+		array (
+			'key' => 'field_resolve',
+			'label' => '現在のトピックの状態',
+			'name' => 'resolve_fields',
+			'type' => 'checkbox',
+			'choices' => array (
+			'resolve' => '解決済み',
+			),
+			'default_value' => '',
+			'layout' => 'vertical',
+		),
+	),
+	'location' => array (
+		array (
+			array (
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'topic',
+				'order_no' => 0,
+				'group_no' => 0,
+			),
+		),
+	),
+	'options' => array (
+		'position' => 'normal',
+		'layout' => 'default',
+		'hide_on_screen' => array (
+		),
+	),
+	'menu_order' => 0,
+));
+}
+
+/*
+	タイトルに解決済み表を示する
+*/
+add_filter('the_title','vk_forum_title_custom');
+function vk_forum_title_custom( $title ) {
+// カスタムフィールドの値を取得
+$resolve = get_field('resolve_fields');
+// チェックが入っていたら
+if ( $resolve ) {
+// タイトルの先頭に [ 解決済 ] を追加
+	$title = single_post_title('[ 解決済 ] ',false);
+}
+// 値を返す
+	return $title;
 }
